25da198c54c12ef29a3c82c975a3c04b5da3b807
Ravi Spigner <585pieces@protonmail.com>
2024-03-21 21:20:35 +0000

    completed Coding Lab: Spring Entity

--- src/main/java/Lab/Model/Product.java
+++ src/main/java/Lab/Model/Product.java
@@ -21,9 +21,11 @@ import java.util.Objects;
  * You can read Spring's documentation on Spring Data JPA & Entities here: https://spring.io/guides/gs/accessing-data-jpa/
  */
 
-// @TODO - Add an annotation to indicate that this class is mapped to a relation in the database:
+// @DONE - Add an annotation to indicate that this class is mapped to a relation in the database:
+@Entity
 public class Product {
-    // TODO - Add an annotation to indicate that this field is the primary key for the corresponding relation:
+    // DONE - Add an annotation to indicate that this field is the primary key for the corresponding relation:
+    @Id
     private long productID;
     private String name;
     private String description;
